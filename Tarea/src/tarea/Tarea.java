31362f43b1816642d93a255a48e6a6e429a3890c
maferito0211 <61156810+maferito0211@users.noreply.github.com>
2020-04-05 23:55:10 -0500

    Add files via upload

---
+++ Tarea/src/tarea/Tarea.java
@@ -0,0 +1,12 @@
+
+package tarea;
+
+
+public class Tarea 
+{
+    public static void main(String[] args) 
+    {
+        FrmJuego frmJuego=new FrmJuego();
+        frmJuego.setVisible(true);
+    } 
+}

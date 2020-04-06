6f306de5ad751c4c7c2d2716f920548abaedbfa7
maferito0211 <61156810+maferito0211@users.noreply.github.com>
2020-04-05 23:56:11 -0500

    Add files via upload

---
+++ Tarea1/src/tarea1/Tarea1.java
@@ -0,0 +1,15 @@
+
+package tarea1;
+
+
+public class Tarea1
+{
+
+    
+    public static void main(String[] args) 
+    {
+        FrmJuego frmJuego=new FrmJuego();
+        frmJuego.setVisible(true);
+    }
+    
+}
